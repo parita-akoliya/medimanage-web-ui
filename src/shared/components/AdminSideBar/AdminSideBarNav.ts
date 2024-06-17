a3d1df836242e2df70223dad9d54c775f9da3186
Utsav Padariya <upadariya1180@conestogac.on.ca>
2024-06-18 02:28:47 +0530

    dummy data removed

--- src/shared/components/AdminSideBar/AdminSideBarNav.ts
+++ src/shared/components/AdminSideBar/AdminSideBarNav.ts
@@ -2,7 +2,7 @@ const AdminSideBarNav = [
     {
       link: "/admin",
       section: "dashboard",
-      icon: "lucide:layout-dashboard", //width:"20"
+      icon: "lucide:layout-dashboard",
       text: "Dashboard",
     },
     {
@@ -11,37 +11,6 @@ const AdminSideBarNav = [
       icon: "icon-park-outline:ad-product",
       text: "Users",
     },
-    {
-      link: "/customers",
-      section: "customers",
-      icon: "ph:users-bold",
-      text: "Customers",
-    },
-    {
-      link: "/orders",
-      section: "orders",
-      icon: "icon-park-outline:transaction-order",
-      text: "Orders",
-    },
-    {
-      link: "/analytics",
-      section: "analytics",
-      icon: "carbon:analytics",
-      text: "Analytics",
-    },
-    {
-      link: "/discount",
-      section: "discount",
-      icon: "nimbus:discount-circle",
-      text: "Discount",
-    },
-    {
-      link: "/inventory",
-      section: "inventory",
-      icon: "ic:round-inventory",
-      text: "Inventory",
-    },
   ];
   
   export default AdminSideBarNav;
-  
